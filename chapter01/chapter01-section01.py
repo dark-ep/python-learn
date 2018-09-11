cadb32818eb9914c5efb2a6018b86acc91c4cfcd
lihongle <lhl_1986@qq.com>
2018-09-11 17:21:19 +0800

    feat:01.01.数据类型和变量

---
+++ chapter01/chapter01-section01.py
@@ -0,0 +1,28 @@
+# 整数，浮点数
+print(1, 0xff00, 1.23, 1.2e-5)
+
+# 字符串
+print('abc', 'I\'m ok.', 'I\'m learning\nPython.', r'\\\t\\')
+
+# 换行处理:py文件下处理
+print('''line1
+line2
+line3''')
+
+# 布尔值
+print(True, 3 > 2, True and False, True or False, not 1 > 2)
+
+# 特殊值
+print(None)
+
+# 参数变换
+a = 'ABC'
+b = a
+a = 'XYZ'
+print(b)
+
+# 运算
+print(10 / 3)
+print(9 / 3)
+print(10 // 3)
+print(10 % 3)
