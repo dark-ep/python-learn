bd35e84be46a7016cf81db07282bec61ca76ec77
lihongle <lhl_1986@qq.com>
2018-10-11 16:40:17 +0800

    feat:02.01.调用函数

---
+++ chapter02/chapter02-section01.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# abs
+print(abs(100))
+print(abs(-20))
+print(abs(12.34))
+
+# max
+print(max(1, 3, 4, 6, 7, 2, 3, 2.5))
+
+# 数据类型转换
+print(int('123'))
+print(int(12.34))
+print(float('12.34'))
+print(str(1.23))
+print(str(100))
+print(bool(1))
+print(bool(''))
+
+# hex
+print(hex(255))
+print(hex(1000))
+
+# 函数指向
+a = abs
+print(a(-1))
