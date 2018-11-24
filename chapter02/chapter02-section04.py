a86b90005bd76cd064c11592ae86b7fe2566243b
lihongle <lhl_1986@qq.com>
2018-11-24 13:04:59 +0800

    feat:02.04.递归函数

--- chapter02/chapter02-section04.py
+++ chapter02/chapter02-section04.py
@@ -2,20 +2,20 @@
 # -*- coding: utf-8 -*-
 
 # 递归函数
-def fact(n):
+def fact1(n):
     if n == 1:
         return 1
-    return n * fact(n - 1)
+    return n * fact2(n - 1)
 
 
-print(fact(1))
-print(fact(5))
+print(fact1(1))
+print(fact1(5))
 
 
 # print(fact(1000)) # 堆栈溢出
 
 
-def fact(n):
+def fact2(n):
     return fact_iter(n, 1)
 
 
@@ -25,8 +25,8 @@ def fact_iter(num, product):
     return fact_iter(num - 1, num * product)
 
 
-print(fact(1))
-print(fact(5))
+print(fact2(1))
+print(fact2(5))
 
 
 # 汉诺塔的移动可以用递归函数非常简单地实现。
