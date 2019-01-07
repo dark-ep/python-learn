f4ea529a79539bbb5bd17ae3353f180d93b1f203
lihongle <lhl_1986@qq.com>
2019-01-07 18:01:50 +0800

    feat:16.01.发送邮件

--- chapter16/chapter16-section01.py
+++ chapter16/chapter16-section01.py
@@ -22,16 +22,42 @@ password = input('Password: ')
 to_addr = input('To: ')
 smtp_server = input('SMTP server: ')
 mail_type = input('Mail type: ')
-if(mail_type == '1'):
+if mail_type == '1':
     msg = MIMEText('<html><body><h1>Hello</h1>' +
                    '<p>send by <a href="http://www.python.org">Python</a>...</p>' +
                    '</body></html>', 'html', 'utf-8')
-if(mail_type == '2'):
+if mail_type == '2':
     image_path = input('Image path: ')
 
     msg = MIMEMultipart()
     # 邮件正文是MIMEText:
     msg.attach(MIMEText('send with file...', 'plain', 'utf-8'))
+
+    filename = os.path.basename(image_path)
+
+    # 添加附件就是加上一个MIMEBase，从本地读取一个图片:
+    with open(image_path, 'rb') as f:
+        # 设置附件的MIME和文件名，这里是png类型:
+        mime = MIMEBase('image', 'png', filename=filename)
+        # 加上必要的头信息:
+        mime.add_header('Content-Disposition', 'attachment', filename=filename)
+        mime.add_header('Content-ID', '<0>')
+        mime.add_header('X-Attachment-Id', '0')
+        # 把附件的内容读进来:
+        mime.set_payload(f.read())
+        # 用Base64编码:
+        encoders.encode_base64(mime)
+        # 添加到MIMEMultipart:
+        msg.attach(mime)
+if mail_type == '3':
+    image_path = input('Image path: ')
+
+    msg = MIMEMultipart('alternative')
+    # 邮件正文是MIMEText:
+    msg.attach(MIMEText('hello', 'plain', 'utf-8'))
+    msg.attach(MIMEText('<html><body><h1>Hello</h1>' +
+                        '<p><img src="cid:0"></p>' +
+                        '</body></html>', 'html', 'utf-8'))
 
     filename = os.path.basename(image_path)
 
@@ -55,7 +81,15 @@ msg['From'] = _format_addr('Python爱好者 <%s>' % from_addr)
 msg['To'] = _format_addr('管理员 <%s>' % to_addr)
 msg['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
 
-server = smtplib.SMTP(smtp_server, 25)
+enc_smtp = input('Enc smtp: ')
+
+if enc_smtp == '1':
+    print('enc smtp...')
+    server = smtplib.SMTP(smtp_server, 587)
+    server.starttls()
+else:
+    print('no enc smtp...')
+    server = smtplib.SMTP(smtp_server, 25)
 server.set_debuglevel(1)
 server.login(from_addr, password)
 server.sendmail(from_addr, [to_addr], msg.as_string())
